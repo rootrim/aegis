1c38ac7c7859924a2a089b007ce7b8eed4c99be4
nixtrim <omuaz01@gmail.com>
2025-08-05 14:17:49 +0300

    space key is now leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,8 +5,8 @@ local opt = vim.o
 
 -- <leader> key. Defaults to `\`. Some people prefer space.
 -- The default leader is '\'. Some people prefer <space>. Uncomment this if you do, too.
--- vim.g.mapleader = ' '
--- vim.g.maplocalleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- See :h <option> to see what the options do
 
@@ -21,7 +21,7 @@ opt.showmatch = true -- Highlight matching parentheses, etc
 opt.incsearch = true
 opt.hlsearch = true
 
-opt.spell = true
+opt.spell = false
 opt.spelllang = 'en'
 
 opt.expandtab = true
