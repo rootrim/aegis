58c0c1226a8564a76dab11280c4895ad340f1a4e
nixtrim <omuaz01@gmail.com>
2025-08-10 14:20:45 +0300

    added more mini thingies

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -3,4 +3,8 @@ if vim.g.did_load_mini_plugin then
 end
 vim.g.did_load_mini_plugin = true
 
-require('mini.basics').setup()
+require('mini.basics').setup { mappings = { windows = true } }
+require('mini.comment').setup()
+require('mini.ai').setup()
+require('mini.move').setup()
+require('mini.keymap').setup()
