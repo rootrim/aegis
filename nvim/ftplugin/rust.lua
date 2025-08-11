da40d45d5dcb3a69eb9f34f3372c436398fac46d
nixtrim <omuaz01@gmail.com>
2025-08-11 15:27:10 +0300

    changed rust keymaps

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,8 +1,8 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set('n', '<leader>ca', function()
+vim.keymap.set('n', 'gra', function()
   vim.cmd.RustLsp('codeAction')
   -- or vim.lsp.buf.codeAction()
-end, { desc = '[C]ode [A]ction', silent = true, buffer = bufnr })
+end, { desc = 'LSP: code action', silent = true, buffer = bufnr })
 vim.keymap.set('n', 'K', function()
   vim.cmd.RustLsp { 'hover', 'actions' }
-end, { silent = true, buffer = bufnr })
+end, { desc = 'LSP: hover', silent = true, buffer = bufnr })
