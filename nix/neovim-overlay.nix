2a9f6110091572436fb31efdb23e08fed398af65
nixtrim <omuaz01@gmail.com>
2025-08-29 18:10:26 +0300

    added wakatime

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -80,6 +80,7 @@ let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    vim-wakatime
   ];
 
   extraPackages = with pkgs; [
