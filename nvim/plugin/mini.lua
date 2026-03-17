3dcdbc93543ff493512eaf1e3ffc024497b32918
nixtrim <omuaz01@gmail.com>
2026-03-17 23:09:48 +0300

    removed lualine

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -18,12 +18,58 @@ require('mini.surround').setup()
 require('mini.jump').setup()
 require('mini.jump2d').setup()
 
-local gen_loader = require('mini.snippets').gen_loader
-require('mini.snippets').setup({
+local function extra_mode_status()
+  -- recording macros
+  local reg_recording = vim.fn.reg_recording()
+  if reg_recording ~= '' then
+    return ' @' .. reg_recording
+  end
+  -- executing macros
+  local reg_executing = vim.fn.reg_executing()
+  if reg_executing ~= '' then
+    return ' @' .. reg_executing
+  end
+  -- ix mode (<C-x> in insert mode to trigger different builtin completion sources)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == 'ix' then
+    return '^X: (^]^D^E^F^I^K^L^N^O^Ps^U^V^Y)'
+  end
+  return ''
+end
+require('mini.statusline').setup {
+  content = {
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+      local git = MiniStatusline.section_git { trunc_width = 40 }
+      local diff = MiniStatusline.section_diff { trunc_width = 75 }
+      local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+      local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+      local filename = MiniStatusline.section_filename { trunc_width = 140 }
+      local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+      local location = tostring(math.floor(vim.fn.line('.') / vim.fn.line('$') * 100)) .. '%%'
+      local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+      local extra_mode_status_label = extra_mode_status()
+
+      return MiniStatusline.combine_groups {
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+        '%<',
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=',
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl, strings = { extra_mode_status_label, search, location } },
+      }
+    end,
+  },
+}
+
+local snippets = require('mini.snippets')
+local gen_loader = snippets.gen_loader
+snippets.setup {
   snippets = {
     gen_loader.from_lang(),
-  }
-})
+  },
+}
 
 local map_multistep = require('mini.keymap').map_multistep
 map_multistep('i', '<CR>', { 'minipairs_cr' })
