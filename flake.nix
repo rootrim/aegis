d7729ad27c7200cd8af495b5543c04c0867d1981
trim <omuaz01@gmail.com>
2026-04-11 18:27:57 +0300

    dropped support for darwin x86_64

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
 
       perSystem = {system, ...}: let
         neovim-overlay = import ./nix/neovim-overlay.nix {inherit inputs;};
