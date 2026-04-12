f8c6829a2fbdf0d004b018e3a9af2ad6144b2803
trim <omuaz01@gmail.com>
2026-04-12 13:54:27 +0300

    update nixd

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -1,2 +1,28 @@
 local lsp = require('user.lsp')
-lsp.enable_and_config('nixd', {})
+lsp.enable_and_config('nixd', {
+	cmd = { 'nixd', '--semantic-tokens=true' },
+	settings = {
+		nixd = {
+			nixpkgs = {
+				expr = 'import <nixpkgs> { }',
+			},
+			formatting = {
+				command = { 'alejandra' },
+			},
+			options = {
+				nixos = {
+					expr = '(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.zenith.options',
+				},
+				home_manager = {
+					expr = '(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.zenith.options.home-manager.users.type.getSubOptions []',
+				},
+				flake_parts = {
+					expr = '(builtins.getFlake (builtins.toString ./.)).debug.options',
+				},
+				flake_parts2 = {
+					expr = '(builtins.getFlake (builtins.toString ./.)).currentSystem.options',
+				},
+			},
+		},
+	},
+})
