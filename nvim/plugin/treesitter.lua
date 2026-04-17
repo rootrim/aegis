a5a66abdef94951c0b667aa23a97e9c8ddf1755b
trim <omuaz01@gmail.com>
2026-04-17 22:48:26 +0300

    added indent for treesitter

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -70,6 +70,7 @@ vim.api.nvim_create_autocmd('FileType', {
 	pattern = { '<filetype>' },
 	callback = function()
 		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end,
 })
 
